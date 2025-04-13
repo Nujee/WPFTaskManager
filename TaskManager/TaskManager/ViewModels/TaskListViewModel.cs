624a1918fe54276f610638e5a9dbe2e062720d99
N-u-j-e-e <alexander.shevelev.v@gmail.com>
2025-04-13 19:33:23 +0300

    Add: main classes for add task functionality

--- TaskManager/TaskManager/ViewModels/TaskListViewModel.cs
+++ TaskManager/TaskManager/ViewModels/TaskListViewModel.cs
@@ -1,16 +1,38 @@
 ﻿using System.Collections.ObjectModel;
+using System.Windows.Input;
+using TaskManager.Helpers;
 using TaskManager.Models;
 using TaskManager.Services;
 
 namespace TaskManager.ViewModels
 {
-    public sealed class TaskListViewModel
+    public sealed class TaskListViewModel : ViewModelBase
     {
+        private readonly ITaskService _taskService;
+
         public ObservableCollection<TaskModel> Tasks { get; }
+
+        public string NewTaskTitle { get; set; }
+
+        public ICommand AddTaskCommand { get; }
 
         public TaskListViewModel(ITaskService taskService)
         {
+            _taskService = taskService;
             Tasks = new ObservableCollection<TaskModel>(taskService.GetTasks());
+
+            AddTaskCommand = new RelayCommand(AddTask);
+        }
+
+        private void AddTask()
+        {
+            if (!string.IsNullOrWhiteSpace(NewTaskTitle))
+            {
+                var task = new TaskModel { Title = NewTaskTitle };
+                Tasks.Add(task);
+                NewTaskTitle = string.Empty;
+                OnPropertyChanged(nameof(NewTaskTitle));
+            }
         }
     }
 }
