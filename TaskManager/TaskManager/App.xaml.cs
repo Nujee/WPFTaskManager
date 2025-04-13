78453aa6b34a9bff17d1a924ecba6d391a3172a6
N-u-j-e-e <alexander.shevelev.v@gmail.com>
2025-04-13 13:18:45 +0300

    Update App.xaml.cs

--- TaskManager/TaskManager/App.xaml.cs
+++ TaskManager/TaskManager/App.xaml.cs
@@ -1,17 +1,31 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Configuration;
-using System.Data;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using Microsoft.Extensions.DependencyInjection;
 using System.Windows;
+using TaskManager.Services;
+using TaskManager.ViewModels;
 
 namespace TaskManager
 {
-    /// <summary>
-    /// Логика взаимодействия для App.xaml
-    /// </summary>
     public partial class App : Application
     {
+        public static ServiceProvider ServiceProvider { get; private set; }
+
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            var serviceCollection = new ServiceCollection();
+            ConfigureServices(serviceCollection);
+            ServiceProvider = serviceCollection.BuildServiceProvider();
+
+            var mainWindow = new MainWindow
+            {
+                DataContext = ServiceProvider.GetRequiredService<TaskListViewModel>()
+            };
+            mainWindow.Show();
+        }
+
+        private void ConfigureServices(IServiceCollection services)
+        {
+            services.AddSingleton<ITaskService, TaskService>();
+            services.AddSingleton<TaskListViewModel>();
+        }
     }
 }
